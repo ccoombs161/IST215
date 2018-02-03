d281a43c2fd20cb55bd59414870045a33377e8e6
ccoombs161 <cameron.coombs97@gmail.com>
2018-02-03 16:56:54 -0500

    Created ExceptionInConstructor

---
+++ Advanced Windows Programming/ExceptionInConstructor/ExceptionInConstructor/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionInConstructor
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                NumberValidator nv = new NumberValidator(5);
+                NumberValidator nv1 = new NumberValidator(11);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+            Console.WriteLine("Program ended successfully");
+        }
+    }
+}
---
+++ Advanced Windows Programming/ExceptionInConstructor/ExceptionInConstructor/NumberValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionInConstructor
+{
+    class NumberValidator
+    {
+       public NumberValidator(int num)
+        {
+            if (num < 11)
+            {
+                Console.WriteLine("Number {0} is valid.", num);
+            }
+            else
+            {
+                string msg =
+                    String.Format("{0} is invalid. Number must be less than or equal to 10.\n", num);
+                throw new ArithmeticException(msg);
+            }
+        }
+    }
+}
