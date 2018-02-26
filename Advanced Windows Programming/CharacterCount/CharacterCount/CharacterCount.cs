56af41b7e6b05b78a78762ba909cc5e65e82cccd
ccoombs161 <cameron.coombs97@gmail.com>
2018-02-26 18:22:08 -0500

    Created CharacterCount

---
+++ Advanced Windows Programming/CharacterCount/CharacterCount/CharacterCount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace CharacterCount
+{
+	class CharacterCount
+	{
+		static void Main(string[] args)
+		{
+			int digits, words, nonwords, whitespace; // character counters
+
+			// get sentence
+			Console.WriteLine("Please enter a sentence:");
+			string sentence = Console.ReadLine();
+
+			digits = Regex.Matches(sentence, @"\d").Count;
+
+			words = Regex.Matches(sentence, @"\w").Count;
+
+			nonwords = Regex.Matches(sentence, @"\W").Count;
+
+			whitespace = Regex.Matches(sentence, @"\s").Count;
+
+			// display results 
+			Console.WriteLine($"\nDigits: {digits}, " +
+				$"word chars: {words}, non-word chars: {nonwords}, whitespace chars: {whitespace}.");
+		} // end main
+	} // end class CHaracterCount
+}
