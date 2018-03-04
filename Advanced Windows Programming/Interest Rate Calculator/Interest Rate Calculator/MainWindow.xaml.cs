8e9e53dbfb66b49d64123fdf529386da1aba3209
ccoombs161 <cameron.coombs97@gmail.com>
2018-03-04 16:16:23 -0500

    Created Interest Rate Calculator

---
+++ Advanced Windows Programming/Interest Rate Calculator/Interest Rate Calculator/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Interest_Rate_Calculator
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			txtPrincipal.Focus();
+		}
+		private void btnCalculate_Click(object sender, RoutedEventArgs e)
+		{
+			decimal amount = 0M;
+			decimal principal = 0M;
+			double interestRate = 0;
+			int years = 0;
+			int nTimesComputed = 0;
+
+			years = Convert.ToInt32(txtYears.Text);
+			interestRate = Convert.ToDouble(txtInterest.Text);
+			principal = Convert.ToDecimal(txtPrincipal.Text);
+			amount = Convert.ToDecimal(txtPrincipal.Text);
+
+			if (rdoMonthly.IsChecked == true)
+				nTimesComputed = 12;
+			else if (rdoQuarterly.IsChecked == true)
+				nTimesComputed = 4;
+			else if (rdoSemiAnnually.IsChecked == true)
+				nTimesComputed = 2;
+			else
+				nTimesComputed = 1;
+
+			amount = principal * (decimal)Math.Pow((1 + ((interestRate / 100) / nTimesComputed)), (nTimesComputed * years));
+
+			txtCurrentValue.Text = amount.ToString("C");
+			decimal interestEarned = amount - principal;
+			txtInterestEarned.Text = interestEarned.ToString("C");
+			txtPrincipal.SelectAll();
+			txtPrincipal.Focus();
+		}
+
+		private void btnClose_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+	}
+}
