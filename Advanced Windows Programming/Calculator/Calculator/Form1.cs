cb65d885902944155fa73a7aa0eeab947c6f7ada
ccoombs161 <cameron.coombs97@gmail.com>
2018-03-28 13:37:53 -0400

    Added functionality to Calculator

--- Advanced Windows Programming/Calculator/Calculator/Form1.cs
+++ Advanced Windows Programming/Calculator/Calculator/Form1.cs
@@ -12,9 +12,188 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+        private static string input;
+        private static List<object> postFixList;
+        double output;
         public Form1()
         {
             InitializeComponent();
+            TextBox1.SelectAll();
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "7";
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "8";
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "9";
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "4";
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "5";
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "6";
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "1";
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "2";
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "3";
+        }
+
+        private void Button0_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "0";
+        }
+
+        private void Button13_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += ".";
+        }
+
+        private void ButtonMult_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "*";
+        }
+
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "+";
+        }
+
+        private void ButtonSub_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "-";
+        }
+
+        private void ButtonDiv_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "/";
+        }
+
+        private void ButtonClear_Click(object sender, EventArgs e)
+        {
+            TextBox1.Clear();
+            TextBox1.Focus();
+        }
+
+        private void ButtonClearEntry_Click(object sender, EventArgs e)
+        {
+            string oldText = TextBox1.Text;
+            TextBox1.Text = oldText.Remove(oldText.Length - 1, 1);
+        }
+
+        private void ButtonLParen_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += "(";
+        }
+
+        private void ButtonRParen_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text += ")";
+        }
+
+        private void ButtonEq_Click(object sender, EventArgs e)
+        {
+            input = TextBox1.Text;
+
+            if (input.Equals("") || string.IsNullOrEmpty(input) || input.Length < 3)
+                return;
+
+            //Convert infix to post fix notation. No error handling
+            // objInpArray = CharToObjectArray(input);
+            List<object> tempList = CharToObjectArray(input);
+            InfixToPostfix converter = new InfixToPostfix(tempList);
+            postFixList = converter.ConvertToPostfix(); // do the translation
+            Console.WriteLine("Postfix:");
+            //DisplaytheList(postFixList);
+
+            //Take the postfix and evaluate the result.
+            ParseAndCalculatePostfix theParser = new ParseAndCalculatePostfix(postFixList);
+            output = theParser.doParse();  // do the evaluation
+            TextBox1.Text += "\r\n= " + output.ToString();
+            //TextBox1.SelectAll();
+
+        }
+
+        public static List<object> CharToObjectArray(string s)
+        {
+            List<object> thisList = new List<object>();
+
+            s = s.Replace("+", " + ");  // it is still a char array or a string
+            s = s.Replace("*", " * ");
+            s = s.Replace("-", " - ");
+            s = s.Replace("/", " / ");
+            s = s.Replace("%", " % ");
+            s = s.Replace("(", " ( ");
+            s = s.Replace(")", " ) ");
+
+            // remove spaces. New string is now a string array instead of char array
+            // it can hold multi-digit numbers. Before it could only hod single digit.
+            string[] inpArr = s.Split(' ');
+
+            // now put content into an object array. Make all numbers double and
+            // sybols and puncuations chars.
+
+            double num;
+            for (int i = 0; i < inpArr.Length; i++)
+            {
+                object temp = inpArr[i];
+
+
+                bool isDouble = Double.TryParse(temp.ToString(), out num);
+                if (isDouble)
+                {
+                    thisList.Add(Convert.ToDouble(inpArr[i]));
+                }
+                else
+                {
+                    thisList.Add(Convert.ToString(inpArr[i]));
+                }
+            }
+
+            // Console.WriteLine("Constructor: New array {0}", objInpArray);
+            return thisList;
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.Delete))
+            {
+                TextBox1.Clear();
+                TextBox1.Focus();
+                TextBox1.SelectAll();
+            }
+
+        }
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- Advanced Windows Programming/Calculator/Calculator/ParseAndCalculatePostFix.cs
+++ Advanced Windows Programming/Calculator/Calculator/ParseAndCalculatePostFix.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,76 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    class ParseAndCalculatePostFix
+    class ParseAndCalculatePostfix
     {
-    }
+        private Stack theStack;
+        private List<object> input;
+        private List<object> output;
+        public double result;
+
+        public ParseAndCalculatePostfix(List<object> s)
+        {
+            theStack = new Stack();
+            input = s;
+            output = new List<object>();
+        }
+
+
+        public double doParse()
+        {
+            theStack = new Stack();      // make the new stack
+            int j;
+            double num1, num2;
+
+            bool res;
+            object temp;
+            for (j = 0; j < input.Count; j++)   // for each char,
+            {
+                //Get a new value. If it is a number, push it on the stack
+                temp = input[j];  //get new value. See if it is a numner
+                double num;     //do this to make sure that it is a number
+                res = Double.TryParse(temp.ToString(), out num);
+                if (res)
+                {
+                    theStack.Push((double)num);
+                }
+                else         // it is not a number. It's an operator
+                {
+                    num2 = (double)theStack.Pop();   // pop 2 operands and do the math and them pop result back.
+                    num1 = (double)theStack.Pop();   // No error checking. if code is written well, num will be double
+
+                    switch (temp.ToString())                // time to do the math
+                    {
+                        case "+":
+                            result = num1 + num2;
+                            Console.Write("{0} + {1} = {2}\n", num1, num2, result);
+                            break;
+                        case "-":
+                            result = num1 - num2;
+                            Console.Write("{0} - {1} = {2}\n", num1, num2, result);
+                            break;
+                        case "*":
+                            result = num1 * num2;
+                            Console.Write("{0} * {1} = {2}\n", num1, num2, result);
+                            break;
+                        case "/":
+                            result = num1 / num2;
+                            Console.Write("{0} / {1} = {2}\n", num1, num2, result);
+                            break;
+                        default:
+                            result = 0;
+                            Console.Write("Error: In Default, ch='{0}'\n", temp.ToString());
+                            break;
+                    }  // end switch                    
+                    theStack.Push(result);        // push result
+                }  // end else
+            }  // end for
+            res = Double.TryParse(theStack.Pop().ToString(), out result);            // get answer
+            if (!(res))
+                Console.WriteLine("Result from the stack was not a double");
+            return result;
+        }  // end doParse()
+
+
+    }  // end class ParsePost
 }
