0fce73870f4e4e189bc4d1fd0dc2dfce40a1f4fc
ccoombs161 <cameron.coombs97@gmail.com>
2018-04-02 20:22:03 -0400

    Created TabbedCheckListBoxForm

---
+++ Advanced Windows Programming/TabbedCheckListBoxForm/TabbedCheckListBoxForm/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace TabbedCheckListBoxForm
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.linksTabControl = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.resultsTab = new System.Windows.Forms.TabPage();
+			this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+			this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+			this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+			this.linkLabel4 = new System.Windows.Forms.LinkLabel();
+			this.linkLabel5 = new System.Windows.Forms.LinkLabel();
+			this.linkLabel6 = new System.Windows.Forms.LinkLabel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.linksTabControl.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// linksTabControl
+			// 
+			this.linksTabControl.Controls.Add(this.tabPage1);
+			this.linksTabControl.Controls.Add(this.tabPage2);
+			this.linksTabControl.Controls.Add(this.resultsTab);
+			this.linksTabControl.Location = new System.Drawing.Point(3, 3);
+			this.linksTabControl.Name = "linksTabControl";
+			this.linksTabControl.SelectedIndex = 0;
+			this.linksTabControl.Size = new System.Drawing.Size(303, 210);
+			this.linksTabControl.TabIndex = 1;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.label1);
+			this.tabPage1.Controls.Add(this.checkedListBox1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 25);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(295, 181);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Includes";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.textBox6);
+			this.tabPage2.Controls.Add(this.textBox5);
+			this.tabPage2.Controls.Add(this.textBox4);
+			this.tabPage2.Controls.Add(this.textBox3);
+			this.tabPage2.Controls.Add(this.textBox2);
+			this.tabPage2.Controls.Add(this.textBox1);
+			this.tabPage2.Controls.Add(this.button6);
+			this.tabPage2.Controls.Add(this.button5);
+			this.tabPage2.Controls.Add(this.button4);
+			this.tabPage2.Controls.Add(this.button3);
+			this.tabPage2.Controls.Add(this.button2);
+			this.tabPage2.Controls.Add(this.button1);
+			this.tabPage2.Controls.Add(this.linkLabel6);
+			this.tabPage2.Controls.Add(this.linkLabel5);
+			this.tabPage2.Controls.Add(this.linkLabel4);
+			this.tabPage2.Controls.Add(this.linkLabel3);
+			this.tabPage2.Controls.Add(this.linkLabel2);
+			this.tabPage2.Controls.Add(this.linkLabel1);
+			this.tabPage2.Location = new System.Drawing.Point(4, 25);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(295, 181);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "URLs";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// resultsTab
+			// 
+			this.resultsTab.Location = new System.Drawing.Point(4, 25);
+			this.resultsTab.Name = "resultsTab";
+			this.resultsTab.Padding = new System.Windows.Forms.Padding(3);
+			this.resultsTab.Size = new System.Drawing.Size(295, 181);
+			this.resultsTab.TabIndex = 2;
+			this.resultsTab.Text = "Links";
+			this.resultsTab.UseVisualStyleBackColor = true;
+			// 
+			// checkedListBox1
+			// 
+			this.checkedListBox1.FormattingEnabled = true;
+			this.checkedListBox1.Location = new System.Drawing.Point(16, 57);
+			this.checkedListBox1.Name = "checkedListBox1";
+			this.checkedListBox1.Size = new System.Drawing.Size(120, 89);
+			this.checkedListBox1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(3, 26);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(257, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Select which links you want to appear";
+			// 
+			// linkLabel1
+			// 
+			this.linkLabel1.AutoSize = true;
+			this.linkLabel1.Location = new System.Drawing.Point(22, 21);
+			this.linkLabel1.Name = "linkLabel1";
+			this.linkLabel1.Size = new System.Drawing.Size(72, 17);
+			this.linkLabel1.TabIndex = 0;
+			this.linkLabel1.TabStop = true;
+			this.linkLabel1.Text = "linkLabel1";
+			// 
+			// linkLabel2
+			// 
+			this.linkLabel2.AutoSize = true;
+			this.linkLabel2.Location = new System.Drawing.Point(22, 49);
+			this.linkLabel2.Name = "linkLabel2";
+			this.linkLabel2.Size = new System.Drawing.Size(72, 17);
+			this.linkLabel2.TabIndex = 1;
+			this.linkLabel2.TabStop = true;
+			this.linkLabel2.Text = "linkLabel2";
+			// 
+			// linkLabel3
+			// 
+			this.linkLabel3.AutoSize = true;
+			this.linkLabel3.Location = new System.Drawing.Point(22, 78);
+			this.linkLabel3.Name = "linkLabel3";
+			this.linkLabel3.Size = new System.Drawing.Size(72, 17);
+			this.linkLabel3.TabIndex = 2;
+			this.linkLabel3.TabStop = true;
+			this.linkLabel3.Text = "linkLabel3";
+			// 
+			// linkLabel4
+			// 
+			this.linkLabel4.AutoSize = true;
+			this.linkLabel4.Location = new System.Drawing.Point(22, 106);
+			this.linkLabel4.Name = "linkLabel4";
+			this.linkLabel4.Size = new System.Drawing.Size(72, 17);
+			this.linkLabel4.TabIndex = 3;
+			this.linkLabel4.TabStop = true;
+			this.linkLabel4.Text = "linkLabel4";
+			// 
+			// linkLabel5
+			// 
+			this.linkLabel5.AutoSize = true;
+			this.linkLabel5.Location = new System.Drawing.Point(22, 132);
+			this.linkLabel5.Name = "linkLabel5";
+			this.linkLabel5.Size = new System.Drawing.Size(72, 17);
+			this.linkLabel5.TabIndex = 4;
+			this.linkLabel5.TabStop = true;
+			this.linkLabel5.Text = "linkLabel5";
+			// 
+			// linkLabel6
+			// 
+			this.linkLabel6.AutoSize = true;
+			this.linkLabel6.Location = new System.Drawing.Point(22, 155);
+			this.linkLabel6.Name = "linkLabel6";
+			this.linkLabel6.Size = new System.Drawing.Size(72, 17);
+			this.linkLabel6.TabIndex = 5;
+			this.linkLabel6.TabStop = true;
+			this.linkLabel6.Text = "linkLabel6";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(214, 15);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(214, 43);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(214, 72);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 8;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(214, 100);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 9;
+			this.button4.Text = "button4";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(214, 129);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 23);
+			this.button5.TabIndex = 10;
+			this.button5.Text = "button5";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(214, 152);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(75, 23);
+			this.button6.TabIndex = 11;
+			this.button6.Text = "button6";
+			this.button6.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(100, 18);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 22);
+			this.textBox1.TabIndex = 12;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(100, 44);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 22);
+			this.textBox2.TabIndex = 13;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(100, 72);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 22);
+			this.textBox3.TabIndex = 14;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(100, 103);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 22);
+			this.textBox4.TabIndex = 15;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(100, 129);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 22);
+			this.textBox5.TabIndex = 16;
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(100, 150);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(100, 22);
+			this.textBox6.TabIndex = 17;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(491, 388);
+			this.Controls.Add(this.linksTabControl);
+			this.Name = "Form1";
+			this.Text = "Tabs with CheckedListBoxes";
+			this.linksTabControl.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage1.PerformLayout();
+			this.tabPage2.ResumeLayout(false);
+			this.tabPage2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TabControl linksTabControl;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.TabPage resultsTab;
+		private System.Windows.Forms.CheckedListBox checkedListBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.LinkLabel linkLabel6;
+		private System.Windows.Forms.LinkLabel linkLabel5;
+		private System.Windows.Forms.LinkLabel linkLabel4;
+		private System.Windows.Forms.LinkLabel linkLabel3;
+		private System.Windows.Forms.LinkLabel linkLabel2;
+		private System.Windows.Forms.LinkLabel linkLabel1;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
+
